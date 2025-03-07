632e35dfcde12cd91fc709637fea3fe49c9f8f89
Jameel <sbijjj1@gmail.com>
2025-03-08 07:39:06 +0800

    Improved UI for Curriculum Details and Next Courses

---
+++ DSA_Group1_Final_Project/Classes/MMCM_Colors.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace DSA_Group1_Final_Project.Classes
+{
+    public static class MMCMColors
+        {
+            public static readonly Color Blue = ColorTranslator.FromHtml("#18325D");  // Delft Blue
+            public static readonly Color Red = ColorTranslator.FromHtml("#EC1F28");   // Red
+            public static readonly Color White = ColorTranslator.FromHtml("#FFFFFF"); // White
+            public static readonly Color Black = ColorTranslator.FromHtml("#000000"); // Black
+            public static readonly Color Silver = ColorTranslator.FromHtml("#BFBEBC"); // Silver
+            public static readonly Color Orange = ColorTranslator.FromHtml("#FF9800"); // Orange
+        }
+    
+}
