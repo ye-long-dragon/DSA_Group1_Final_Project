a30a4be11b127d817df0516faa32bb5a9ee8f16c
Jameel <sbijjj1@gmail.com>
2025-03-08 11:03:12 +0800

    fixed the disappearing header problem in view curriculum details

--- DSA_Group1_Final_Project/Windows/UserControls/Admin/ViewCurriculumDetails.Designer.cs
+++ DSA_Group1_Final_Project/Windows/UserControls/Admin/ViewCurriculumDetails.Designer.cs
@@ -28,17 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
+            lblStudentInfo = new Label();
             SuspendLayout();
+            // 
+            // lblStudentInfo
+            // 
+            lblStudentInfo.AutoSize = true;
+            lblStudentInfo.Location = new Point(22, 78);
+            lblStudentInfo.Name = "lblStudentInfo";
+            lblStudentInfo.Size = new Size(50, 20);
+            lblStudentInfo.TabIndex = 0;
+            lblStudentInfo.Text = "label1";
             // 
             // ViewCurriculumDetails
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(lblStudentInfo);
             Name = "ViewCurriculumDetails";
-            Size = new Size(527, 487);
+            Size = new Size(799, 707);
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label lblStudentInfo;
     }
 }
